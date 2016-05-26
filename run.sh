3bb062493cfe7c3770af4add8e446bb2b3d75668
Andy Postnikov <apostnikov@gmail.com>
2016-05-26 22:19:37 +0300

    use 3.3 and php 5.6

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-drush dl drupal-8.1.x --dev --drupal-project-rename
-cp .ht.router.php drupal/
+mkdir -p drupal
 
+docker-compose -f docker-compose.yml stop
+yes | docker-compose -f docker-compose.yml rm
 docker-compose up -d
+
+docker exec -it dockerphp_web_1 drush dl drupal --destination=/srv
+docker exec -it dockerphp_web_1 drush si --db-url=mysql://d8:d8@mysql/d8 --account-pass=admin -y
+#drush dl drupal-8.1.x --dev --drupal-project-rename
+cp .ht.router.php drupal/
+wget https://www.adminer.org/static/download/4.2.4/adminer-4.2.4-en.php -O drupal/adminer.php
 
 #docker run -e MYSQL_ROOT_PASSWORD=d8 -e MYSQL_DATABASE=proconsort -e MYSQL_USER=d8 -e MYSQL_PASSWORD=d8 -v $(pwd)/db:/var/lib/mysql -d --name mysql percona:5.6.27
 
